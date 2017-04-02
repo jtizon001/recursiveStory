584ffc48aafe65bb80a3197e840bf24e13fb01e7
jtizon001 <tizon.jonathan@gmail.com>
2017-04-02 00:47:14 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/reclLab10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reclLab10.dir/main.c.o"
+  "reclLab10.pdb"
+  "reclLab10"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/reclLab10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
